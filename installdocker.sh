6055712117ee227aead38e77628f1c2ef49f226b
Firstruner <35631831+firstruner@users.noreply.github.com>
2024-02-23 18:23:00 +0100

    Update installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y apt-transport-https
+sudo apt-get install -y apt-transport-https git
 sudo apt-get install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
