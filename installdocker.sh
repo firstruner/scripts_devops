4ed4b001999406530d4b1819cdd134f0c6de25da
Firstruner <35631831+firstruner@users.noreply.github.com>
2024-02-23 18:30:50 +0100

    Update installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -1,7 +1,10 @@
+echo "[[[   - INSTALLATION DES PACKAGES -   ]]]"
 sudo apt-get update
 sudo apt-get install -y apt-transport-https git
 sudo apt-get install -y docker.io
+echo "[[[   - DEMARRAGE DU SERVICE -   ]]]"
 sudo systemctl start docker
 sudo systemctl enable docker
+echo "[[[   - PARAMETRAGE DU SERVICE -   ]]]"
 sudo setfacl --modify user:$USER:rw /var/run/docker.sock
 sudo systemctl status docker
