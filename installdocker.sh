345556663a012e63de48dd38220b0326b5c5a851
Firstruner <35631831+firstruner@users.noreply.github.com>
2024-02-23 18:22:07 +0100

    Initial

---
+++ installdocker.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo setfacl --modify user:$USER:rw /var/run/docker.sock
+sudo systemctl status docker
---
+++ installkubernetes.sh
@@ -0,0 +1,15 @@
+sudo apt-get install -y apt-transport-https ca-certificates curl
+curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl kubelet kubeadm
+sudo systemctl enable kubelet
+sudo kubeadm init
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl create -f ./scripts_devops/calico.yaml
+swapoff -a
+kubectl version
+kubectl get pods
