67627a8e93a38f87e375a214bfc8d49fcd7e84bf
Firstruner <35631831+firstruner@users.noreply.github.com>
2024-02-24 16:36:42 +0100

    Update installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -1,10 +1,21 @@
+echo
 echo "[[[   - INSTALLATION DES PACKAGES -   ]]]"
+echo
 sudo apt-get update
 sudo apt-get install -y apt-transport-https
 sudo apt-get install -y docker.io
+echo
 echo "[[[   - DEMARRAGE DU SERVICE -   ]]]"
+echo
 sudo systemctl start docker
 sudo systemctl enable docker
+echo
 echo "[[[   - PARAMETRAGE DU SERVICE -   ]]]"
+echo
 sudo setfacl --modify user:$USER:rw /var/run/docker.sock
-sudo systemctl status docker
+sudo systemctl status docker | egrep eunning;
+echo
+echo "[[[   - RECUPERATION D'IMAGES -   ]]]"
+echo
+docker pull hello-world
+docker pull httpd
