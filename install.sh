8b0518992c1fbe9ba61855bf2692d60b840e5834
Firstruner <35631831+firstruner@users.noreply.github.com>
2024-02-24 12:43:38 +0100

    Add GParted

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo "[[[   - Récupération de Git -   ]]]"
 echo
 sudo apt-get update
-sudo apt-get install -y git
+sudo apt-get install -y git gparted
 cd $HOME
 git clone https://github.com/firstruner/scripts_devops.git
 bash $HOME/scripts_devops/installdocker.sh
