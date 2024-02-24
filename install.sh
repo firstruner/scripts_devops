d166bfa5fc6dc0c98156c608e421a689cd3244fb
Firstruner <35631831+firstruner@users.noreply.github.com>
2024-02-24 16:47:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,18 +9,18 @@ echo
 echo
 echo "[[[   - PARAMETRAGE DU SYSTEME -   ]]]"
 echo
-read -p "-->> Voulez-vous ajuster la taille de la partition ? " -n 1 -r
+read -p "-->> Voulez-vous ajuster la taille de la partition [y/n] ? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]
   sudo gparted
 fi
 echo
-read -p "-->> Voulez-vous installer Docker maintenant ? " -n 1 -r
+read -p "-->> Voulez-vous installer Docker maintenant [y/n] ? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]
   bash $HOME/scripts_devops/installdocker.sh
   echo
-  read -p "-->> Voulez-vous installer Kubernetes maintenant ? " -n 1 -r
+  read -p "-->> Voulez-vous installer Kubernetes maintenant [y/n] ? " -n 1 -r
   echo    # (optional) move to a new line
   if [[ $REPLY =~ ^[Yy]$ ]
     bash $HOME/scripts_devops/installkubernetes.sh
