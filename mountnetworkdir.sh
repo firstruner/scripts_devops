11c20e63eb987cd0d6d780ea793460ffa3896e1b
Firstruner <35631831+firstruner@users.noreply.github.com>
2024-02-25 13:28:11 +0100

    Update mountnetworkdir.sh

--- mountnetworkdir.sh
+++ mountnetworkdir.sh
@@ -1,3 +1,21 @@
+clear
+echo
+echo "[[[   - Récupération d'informations -   ]]]"
+echo
+echo -n "Entrer le chemin réseau (ex.: //192.168.0.1/SharedFolder) : "
+read -r sharedpath
+echo
+echo -n "Entrer le chemin local (ex.: /mnt/monshared) : "
+read -r localpath
+echo
+echo -n "Entrer le nom d'utilisateur : "
+read -r uname
+echo
+echo -n "Entrer le mot de passe : "
+read -r upwd
+echo
+echo -n "Entrer le domaine : "
+read -r udomain
 echo
 echo "[[[   - Mise à jour du système -   ]]]"
 echo
@@ -10,5 +28,7 @@ apt-get install -y cifs-utils psmisc nano
 echo
 echo "[[[   - Montage -   ]]]"
 echo
-mkdir /mnt/shared
-mount -t cifs //IP/SharedFolder /mnt/shared -o username=user,password=pass,domain=dom
+mkdir $localpath
+mount -t cifs $sharedpath $localpath -o username=$uname,password=$upwd,domain=$udomain
+cd $localpath
+ls
